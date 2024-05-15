4e5933e8a5a775e3148950e97824a5e73af241ca
Richardx10 <richardx10@gmail.com>
2024-05-16 00:07:55 +0200

    Svaret bliver nu gemt i databasen

--- Javascript/index.js
+++ Javascript/index.js
@@ -18,44 +18,53 @@ new Vue({
       this.fetchQuestions();
     },
 
+    fetchQuestions() {
+      axios
+        .get(baseURL)
+        .then((response) => {
+          this.questions = response.data;
+          this.getRandomQuestion();
+        })
+        .catch((error) => {
+          console.error("Error fetching questions:", error);
+        });
+    },
+
     getRandomQuestion() {
-      // Hvis der er spørgsmål, så vælg et tilfældigt spørgsmål
       if (this.questions.length > 0) {
-        // Vælg et tilfældigt spørgsmål
         const randomIndex = Math.floor(Math.random() * this.questions.length);
-        // Returner hele spørgsmålsobjektet
         this.currentQuestion = this.questions[randomIndex];
+      } else {
+        console.error("ingen spørgsmål fundet");
       }
     },
 
     submitAnswer(option) {
-        const question = this.currentQuestion;
-        axios
-          .post(`${baseURL}/SubmitAnswer`, {
-            QuestionId: question.QuestionId,
+      const question = this.currentQuestion;
+      console.log("Submitting answer for question:", this.currentQuestion);
+      axios
+        .post(
+          `${baseURL}/SubmitAnswer`,
+          {
+            QuestionId: this.currentQuestion.questionId,
             Option: option,
-          })
-          .then((response) => {
-            this.getRandomQuestion();
-          })
-          .catch((error) => {
-            console.error("Error submitting the answer:", error);
-          });
-      },
+          },
+          {
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        )
+        .then((response) => {
+          console.log("Answer submitted successfully:", response.data);
+          this.getRandomQuestion();
+        })
+        .catch((error) => {
+          console.error("Error submitting the answer:", error);
+        });
+    },
 
-      fetchQuestions() {
-        axios
-          .get(baseURL)
-          .then((response) => {
-            this.questions = response.data;
-            this.getRandomQuestion();
-          })
-          .catch((error) => {
-            console.error("Error fetching questions:", error);
-          });
-      },
-
-    fetchStatistics() {
+    /*     fetchStatistics() {
       axios
         .get(
           `https://restopinionpoll.azurewebsites.net/api/Statistics?questionId=${questionId}`
@@ -66,11 +75,10 @@ new Vue({
         .catch((error) => {
           console.error("Error fetching the statistics:", error);
         });
-    },
-
+    }, */
   },
   watch: {
-    selectedLanguage: function() {
+    selectedLanguage: function () {
       this.getRandomQuestion();
     },
   },
